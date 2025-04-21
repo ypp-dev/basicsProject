c9f09cdf3b0712d9d61d357a6861fbc625e9d18a
yaopangpang <2668829510@qq.com>
2025-04-21 10:33:15 +0800

    新增Websocket工具类

--- core/webSocket/src/main/java/com/ypp/websocket/WebSocketDemo.kt
+++ core/webSocket/src/main/java/com/ypp/websocket/WebSocketDemo.kt
@@ -1,18 +1,20 @@
 package com.ypp.websocket
 
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import java.net.URI
 
 class WebSocketDemo {
     val webSocketConnect by lazy { WebSocketConnect.getInstance(URI("ws:xxxxxxxx")) }
+    var disposable: Disposable?=null
     fun reception(){
         //接收处
         //按需接收选择线程
-        webSocketConnect.webSocketFlowable
+        disposable=webSocketConnect.webSocketFlowable
             .observeOn(Schedulers.io())
             .subscribeOn(Schedulers.io())
             .subscribe ({
-                //如果具体到某个Activity，可以用
+                //如果具体到某个Activity，可以用lifecycle判断是否处理
 
             },{
 
@@ -20,6 +22,9 @@ class WebSocketDemo {
     }
     fun send(message:String){
         webSocketConnect.sendMessage(message)
+    }
+    fun onRelease(){
+        disposable?.dispose()
     }
 
 }
