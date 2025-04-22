dcd50e83f22a8825f3a25f3136b21507d057145f
yaopangpang <2668829510@qq.com>
2025-04-22 14:23:49 +0800

    更新README.md

--- README.md
+++ README.md
@@ -1,7 +1,31 @@
 # Android 官方推荐架构思想实现，核心为数据流+多Repository+MVI
 
+## 使用到的技术
+1. Coroutine + Flow
+2. Hilt
+3. Compose
+4. Room
+5. DataSource
+6. ViewModel
+7. Lifecycle
+
+## 分层职责
+![分层职责](responsibility.png)
+
+# BasicsProject
+
 ## 项目结构
-![项目结构](whiteboard_exported_image.png)
+
+1. Coroutine + Flow
+2. Hilt
+3. Compose
+4. Room
+5. DataSource
+6. ViewModel
+7. Lifecycle
 
 ## 架构流程图
 ![架构流程图](android_state_flow.png)
+## MVI or MVX？
+在目前Android的架构下，不论是MVX都可以很好的融入到现在的体系中，不论下游如何实现data（domain）的实现都可以不受影响。
+
--- core/data/src/main/java/com/ypp/data/repository/HomeRepository.kt
+++ core/data/src/main/java/com/ypp/data/repository/HomeRepository.kt
@@ -1,6 +1,8 @@
 package com.ypp.data.repository
 
 import com.ypp.datastore.UserInfo
+import com.ypp.model.BannerBean
+import com.ypp.model.TopJsonBean
 import com.ypp.model.datastore.Banners
 import kotlinx.coroutines.flow.Flow
 
@@ -9,9 +11,9 @@ import kotlinx.coroutines.flow.Flow
 * */
 interface HomeRepository {
     fun banner():Flow<Banners>
-    fun topJson():Flow<com.ypp.model.TopJsonBean>
+    fun topJson():Flow<TopJsonBean>
     fun userInfo():Flow<List<UserInfo>>
-    suspend fun updateBanner():Result<com.ypp.model.BannerBean>
+    suspend fun updateBanner():Result<BannerBean>
     suspend fun updateTopJson(succeed:()->Unit,fail:()->Unit)
     suspend fun addUserInfo(userInfo: UserInfo)
 
